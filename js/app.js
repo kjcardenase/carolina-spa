a97ce5781af3d3aed7e348497593951576f4b286
Keyla Juleyma Cardenas Esteban <keyla.16f@gmail.com>
2022-06-20 12:11:59 -0500

    terminacion

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.swiper = new Swiper({
+el:'.slider__contenedor',
+slideClass:'slider__slide',
+createElements: true, 
+autoplay:{
+    delay:4000
+},
+loop: true,
+pagination:true,
+navigation: true
+
+});
